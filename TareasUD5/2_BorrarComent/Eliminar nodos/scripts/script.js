104b987acec759920f3fa62911b37114e2d6f6c0
Gustavo_FM <a19gustavofm@iessanclemente.net>
2022-02-05 12:26:42 +0100

    si

--- TareasUD5/2_BorrarComent/Eliminar nodos/scripts/script.js
+++ TareasUD5/2_BorrarComent/Eliminar nodos/scripts/script.js
@@ -10,42 +10,60 @@
 
 const formulario=document.querySelector("form")
 const comentarios=document.querySelector("#comentarios")
-const borrar=document.querySelector("#deleteNode")
+//const borrar=document.querySelector("#deleteNode")
+const textArea=document.querySelector("#textArea")
 const a=document.querySelector("a")
 //const btn=formulario.querySelector("input")
 
-contador=1
 const articles=[]
+function mostrarArticulos(){
+    comentarios.textContent=""
+    articles.forEach((article,indice) => {
+        const articlectl=document.createElement("article")
+        const h4=document.createElement("h4")
+        const p=document.createElement("p")
+        const cabecera=document.createTextNode(`Comentario ${indice+1}`)
+        const texto=document.createTextNode(article.texto)
+        
+        h4.appendChild(cabecera)
+        p.appendChild(texto)
+        articlectl.appendChild(h4)
+        articlectl.appendChild(p)
+        comentarios.appendChild(articlectl)
+     });
+    console.log(articles)
+    textArea.focus()
+    formulario.reset()
+}
+
 formulario.addEventListener('submit', e=>{
     e.preventDefault()
-    const article=document.createElement("article")
-    article.className=contador
-    const h4=document.createElement("h4")
-    const p=document.createElement("p")
-    const cabecera=document.createTextNode(`Comentario ${contador}`)
-    const texto=document.createTextNode(textArea.value)
-    contador++
-    h4.appendChild(cabecera)
-    p.appendChild(texto)
-    article.appendChild(h4)
-    article.appendChild(p)
-    const conjunto=comentarios.appendChild(article)
-    articles.push(conjunto)
-    console.log(articles)
+    articulo={
+        id:new Date().getTime(),
+        texto: textArea.value,
+    }
+    articles.push(articulo)
+    mostrarArticulos()
 })
 
 a.addEventListener("click", e=>{
-    for(let i=contador; i>=0;i--){
+    /*for(let i=contador; i>=0;i--){
     articleBorrar=document.querySelector("article")
     articleClass=articleBorrar.querySelector(".contador")
-    console.log(articleClass)
+    
     //h4=articleBorrar.querySelector("h4")
     //cabecera=h4.querySelector(h4.value)
     //articles.pop()
     if(i==articleClass)
         articleBorrar.remove()
     contador--
-    }
-
+    }*/
+    articles.pop()
+    mostrarArticulos()
+ // for(let i=contador; i>=0;i--){
+   //   document.write(articles[i])
+ // }
+    
+console.log(articles)
 })
 
---
+++ TareasUD5/3_InsertComent/Insercion de nodos/scripts/script.js
@@ -0,0 +1,7 @@
+/*article>h4>p*/
+
+const formulario=document.querySelector("form")
+const textArea=document.querySelector("#textArea")
+const check=formulario.querySelector("input")
+
+

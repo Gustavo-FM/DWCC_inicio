4a71f48e8ff0f071ab84fd07ad22268782a68eb4
Gustavo_FM <a19gustavofm@iessanclemente.net>
2022-02-18 13:11:46 +0100

    Script 4_BorrarComentarioEspecirfico-Error en linea 23

---
+++ TareasUD5/4_BorrarComentEspcfco/Eliminar nodos especificos/scripts/script.js
@@ -0,0 +1,26 @@
+
+
+const formulario=document.querySelector("form")
+const textArea=document.querySelector("#textArea")
+const check=document.getElementsByName("nodeAction")
+const numeroComentario=document.querySelector("#grafCount")
+const temp=document.querySelectorAll("#template").content
+const comentarios=document.querySelectorAll("#comentarios")
+
+const articulos=[]
+const contador=1
+
+function pintarPantalla(){
+    comentarios.innerHTML=""
+    const article=temp.cloneNode(true)
+    h4=article.querySelector("h4")
+    h4.innerHTML=`Comentario ${contador}`
+    p=article.querySelector("p")
+    p.textContent=textArea.value 
+
+}
+
+formulario.addEventListener("submit",e=>{
+    e.preventDefault()
+    pintarPantalla()
+})

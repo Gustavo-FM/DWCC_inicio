d2a88cf5cb3aba4aaad22d596594c0f5f0317cef
Gustavo_FM <a19gustavofm@iessanclemente.net>
2022-02-22 14:18:53 +0100

    No se porque no incluyeh4 y p en article

--- TareasUD5/4_BorrarComentEspcfco/Eliminar nodos especificos/scripts/script.js
+++ TareasUD5/4_BorrarComentEspcfco/Eliminar nodos especificos/scripts/script.js
@@ -4,20 +4,23 @@ const formulario=document.querySelector("form")
 const textArea=document.querySelector("#textArea")
 const check=document.getElementsByName("nodeAction")
 const numeroComentario=document.querySelector("#grafCount")
-const temp=document.querySelectorAll("#template").content
-const comentarios=document.querySelectorAll("#comentarios")
+const temp=document.querySelector("#template").content
+const comentarios=document.querySelector("#comentarios")
 
 const articulos=[]
-const contador=1
+let contador=1
 
 function pintarPantalla(){
-    comentarios.innerHTML=""
+   // comentarios.innerHTML=""
     const article=temp.cloneNode(true)
     h4=article.querySelector("h4")
     h4.innerHTML=`Comentario ${contador}`
     p=article.querySelector("p")
     p.textContent=textArea.value 
-
+    contador++
+    article.appendChild(h4)
+    article.appendChild(p)
+    comentarios.appendChild(article)
 }
 
 formulario.addEventListener("submit",e=>{
